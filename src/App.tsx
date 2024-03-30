96de68ec96a9ada2356417ba4dde561e09be43d7
kshetritej <tejghartikshetri@gmail.com>
2024-03-30 10:44:43 +0545

    chore: moved landing page layout to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import LandingPage from "./components/pages/LandingPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PersonalInfoPage from "./components/pages/PersonalInfoPage";
 import Navbar from "./components/molecules/Navbar";
 import Footer from "./components/molecules/Footer";
+import Header from "./components/molecules/Header";
 function App() {
   return (
     <>
-    <Navbar/>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/new" element={<PersonalInfoPage />} />
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer/>
+      <div className="h-[100vh] flex flex-col">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="content my-auto">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/new" element={<PersonalInfoPage />} />
+              <Route path="/" element={<Header/>} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+
+        <div className="footer absolute bottom-0 mx-auto w-[100%] p-4">
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
