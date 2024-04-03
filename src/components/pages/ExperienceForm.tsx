edc9986742ae60ce4e9f4fcc9624927c528eaa2b
kshetritej <tejghartikshetri@gmail.com>
2024-04-03 22:45:21 +0545

    add: experience form

--- src/components/pages/ExperienceForm.tsx
+++ src/components/pages/ExperienceForm.tsx
@@ -1,10 +1,141 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Textarea } from "../ui/textarea";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  company: z.string(),
+  position: z.string(),
+  startYear: z.string().datetime(),
+  endYear: z.string().datetime(),
+  description: z.string(),
+});
 
 const ExperienceForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {},
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <div>
-Exform      
-    </div>
-  )
-}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="company"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>Company where you worked.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-export default ExperienceForm
+          <FormField
+            control={form.control}
+            name="position"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                Position
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                  {/* Name of the place where you attended your education. */}
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+
+
+          <FormField
+            control={form.control}
+            name="startYear"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+            From 
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                From when did you started working with the company
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+
+          <FormField
+            control={form.control}
+            name="endYear"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                To
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormDescription>
+                When was your last day in this particular position.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                Description
+                </FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+                <FormDescription>
+                  {/* Name of the place where you attended your education. */}
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default ExperienceForm;
